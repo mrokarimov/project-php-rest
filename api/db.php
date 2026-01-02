7ccc5b4a1c704bd475884e6ead540ee91838a2eb
Ozodbek Karimov <97459027+mrokarimov@users.noreply.github.com>
2026-01-02 21:14:08 +0100

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,21 +1,21 @@
 <?php
 header("Content-Type: application/json");
 
-$host = "dpg-d5clua3uibrs73crl27g-a";
-$port = "5432";
-$db   = "final_project_db_0g4f";
-$user = "final_project_db_0g4f_user";
-$pass = "NSZ83w7kEL1P33d1ghJhyKEV39syuyRH";
-
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
-    $conn = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
+    $conn = new PDO(
+        "pgsql:host=dpg-d5c1ua3uibrs73crl27g-a;port=5432;dbname=final_project_db_0g4f",
+        "final_project_db_0g4f_user",
+        "NSZ83w7kEL1P33d1ghJhyKEV39syuyRH",
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode([
-        "error" => $e->getMessage()
+        "error" => "DB connection failed",
+        "details" => $e->getMessage()
     ]);
     exit;
 }
