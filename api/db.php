436c80220246bc8963ff19056bfa6d454b5b5b4b
Ozodbek Karimov <97459027+mrokarimov@users.noreply.github.com>
2026-01-02 21:32:23 +0100

    Update db.php

--- api/db.php
+++ api/db.php
@@ -1,16 +1,24 @@
 <?php
 header("Content-Type: application/json");
 
+$host = getenv("dpg-d5clua3uibrs73crl27g-a");
+$port = getenv("5432");
+$db   = getenv("final_project_db_0g4f");
+$user = getenv("final_project_db_0g4f_user");
+$pass = getenv("NSZ83w7kEL1P33d1ghJhyKEV39syuyRH");
+
+if (!$host || !$port || !$db || !$user || !$pass) {
+    http_response_code(500);
+    echo json_encode(["error" => "Missing DB env variables"]);
+    exit;
+}
+
 try {
-    $conn = new PDO(
-        "pgsql:host=dpg-d5c1ua3uibrs73crl27g-a;port=5432;dbname=final_project_db_0g4f",
-        "final_project_db_0g4f_user",
-        "NSZ83w7kEL1P33d1ghJhyKEV39syuyRH",
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]
-    );
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
+    $conn = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode([
