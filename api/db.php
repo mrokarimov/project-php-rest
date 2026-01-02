cb084da46dddddb9b8511bd06870639014710f7e
Ozodbek Karimov <97459027+mrokarimov@users.noreply.github.com>
2026-01-02 22:01:14 +0100

    Update db.php

--- api/db.php
+++ api/db.php
@@ -17,11 +17,18 @@ if (!$host || !$port || !$db || !$user || !$pass) {
 }
 
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$db";
-    $conn = new PDO($dsn, $user, $pass, [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
+    $dsn = "pgsql:host=$host;port=$port;dbname=$db;sslmode=require";
+
+    $conn = new PDO(
+        $dsn,
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+
 } catch (PDOException $e) {
     http_response_code(500);
     echo json_encode([
